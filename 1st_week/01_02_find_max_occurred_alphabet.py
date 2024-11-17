6533c9db4efd340974b3e1bb02e209b6219aa6b7
dohun <gkgk586@gmail.com>
2024-11-17 21:02:32 +0900

    변경

---
+++ 1st_week/01_02_find_max_occurred_alphabet.py
@@ -0,0 +1,9 @@
+def find_max_occurred_alphabet(string):
+    # 이 부분을 채워보세요!
+    return "a"
+
+
+result = find_max_occurred_alphabet
+print("정답 = i 현재 풀이 값 =", result("hello my name is dingcodingco"))
+print("정답 = e 현재 풀이 값 =", result("we love algorithm"))
+print("정답 = b 현재 풀이 값 =", result("best of best youtube"))
