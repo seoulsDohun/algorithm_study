22b2f138c6e39f976ccc844f39f0cabb63fe1878
dohun <gkgk586@gmail.com>
2024-11-17 22:26:15 +0900

    빅오 빅오메가 표기법 예시로 학습

--- 1st_week/01_02_find_max_occurred_alphabet.py
+++ 1st_week/01_02_find_max_occurred_alphabet.py
@@ -32,3 +32,5 @@ result = find_max_occurred_alphabet
 print("정답 = i 현재 풀이 값 =", result("hello my name is dingcodingco"))
 print("정답 = e 현재 풀이 값 =", result("we love algorithm"))
 print("정답 = b 현재 풀이 값 =", result("best of best youtube"))
+
+
