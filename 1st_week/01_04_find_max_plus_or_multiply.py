acf6e5ae0115c72867a09fb22c42878c5af2b19e
dohun <gkgk586@gmail.com>
2024-11-17 23:16:18 +0900

    O(n) 예시 문제 풀기

---
+++ 1st_week/01_04_find_max_plus_or_multiply.py
@@ -0,0 +1,15 @@
+def find_max_plus_or_multiply(array):
+    max_num = 0
+    for num in array:
+        if max_num <=1 or num <=1 :
+            max_num += num
+        else :
+            max_num *= num
+
+    return max_num
+
+
+result = find_max_plus_or_multiply
+print("정답 = 728 현재 풀이 값 =", result([0,3,5,6,1,2,4]))
+print("정답 = 8820 현재 풀이 값 =", result([3,2,1,5,9,7,4]))
+print("정답 = 270 현재 풀이 값 =", result([1,1,1,3,3,2,5]))
